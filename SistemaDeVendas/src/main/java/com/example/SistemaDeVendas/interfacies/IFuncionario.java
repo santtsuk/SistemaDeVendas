25a3c5ed7cdb72ca703132810de710e4a9bfe847
Deivid Victor <146492249+DeividSenai@users.noreply.github.com>
2024-10-03 19:36:04 -0300

    Adição da Application

--- SistemaDeVendas/src/main/java/com/example/SistemaDeVendas/interfacies/IFuncionario.java
+++ SistemaDeVendas/src/main/java/com/example/SistemaDeVendas/interfacies/IFuncionario.java
@@ -2,13 +2,13 @@ package com.example.SistemaDeVendas.interfacies;
 
 import com.example.SistemaDeVendas.entities.Funcionario;
 
-import java.util.ArrayList;
+import java.util.List;
 
 public interface IFuncionario {
 
     public Funcionario buscarPorId(int id);
 
-    public ArrayList<Funcionario> buscarTodos();
+    public List<Funcionario> buscarTodos();
 
     public void salvar(int id, String nome, String cpf, String cargo, String setor, String dataAdimissao);
 
