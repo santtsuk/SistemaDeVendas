5e4b244ff6c41e8ada9e0b39b37a9acabb92a04d
H-Saimon <147450576+H-Saimon@users.noreply.github.com>
2024-10-08 18:08:59 -0300

    Add files via upload

---
+++ SistemaDeVendas/src/main/java/com/example/SistemaDeVendas/controller/UsuarioController.java
@@ -0,0 +1,53 @@
+package com.example.SistemaDeVendas.controller;
+
+import com.example.SistemaDeVendas.entities.Usuario;
+import com.example.SistemaDeVendas.facades.UsuarioFacade;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class UsuarioController {
+    private UsuarioFacade usuarioFacade;
+
+    @Autowired
+    public UsuarioController(UsuarioFacade usuarioFacade) {
+        this.usuarioFacade = usuarioFacade;
+    }
+
+    @GetMapping("/buscarUsuario")
+    public ResponseEntity<List<Usuario>> buscarTodos() {
+        List<Usuario> usuarios = usuarioFacade.buscarTodos();
+
+        return ResponseEntity.ok(usuarios);
+    }
+
+    @GetMapping("/buscarUsuario/{id}")
+    public ResponseEntity<Usuario> buscarPorId(@PathVariable int id) {
+        Usuario usuario = usuarioFacade.buscarPorId(id);
+
+        return ResponseEntity.ok(usuario);
+    }
+
+    @PostMapping
+    public ResponseEntity<Void> salvar(@RequestBody Usuario usuario) {
+        usuarioFacade.salvar(usuario);
+        return new ResponseEntity(null, HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Usuario> atualizar(@PathVariable int id, @RequestBody Usuario usuario) {
+        usuarioFacade.atualizar(id, usuario);
+
+        return ResponseEntity.ok(usuario);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deletar(@PathVariable int id) {
+        usuarioFacade.deletar(id);
+        return ResponseEntity.ok(null);
+    }
+}
