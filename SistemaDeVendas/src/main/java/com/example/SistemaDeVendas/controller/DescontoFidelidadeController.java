6c306c833de4efe090677edb41fec68a97a96347
Caroline Rocha <146491576+CarolRN1611@users.noreply.github.com>
2024-10-08 21:09:50 -0300

    Update DescontoFidelidadeController.java

--- SistemaDeVendas/src/main/java/com/example/SistemaDeVendas/controller/DescontoFidelidadeController.java
+++ SistemaDeVendas/src/main/java/com/example/SistemaDeVendas/controller/DescontoFidelidadeController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/descontofidelidade")
+@RequestMapping("/descontoFidelidade")
 public class DescontoFidelidadeController {
 
     private DescontoFidelidadeFacade descontoFidelidadeFacade;
