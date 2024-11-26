c98018cf1424bd4f4bdf1273420838e18557b211
santtsuk <marlon.j.santos@ba.estudante.senai.br>
2024-11-25 21:25:19 -0300

    Alterações dentro da class

--- SistemaDeVendas/src/main/java/com/example/SistemaDeVendas/configs/SecurityConfig.java
+++ SistemaDeVendas/src/main/java/com/example/SistemaDeVendas/configs/SecurityConfig.java
@@ -4,13 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
-import org.springframework.security.config.annotation.web.CsrfDsl;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
@@ -106,7 +100,7 @@ public class SecurityConfig {
                 
 
 //--------------------------CONFIRMAÇÃO DE ACESSO DO USUARIO
-//                .requestMatchers("/usuario/**").permitAll();
+                    .requestMatchers("/usuario/**").permitAll();
 
 
         });
